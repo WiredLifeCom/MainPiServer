61a1526cb67b6c1970c866e363c542ecfda87c59
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-05-13 08:57:12 +0200

    Updated test file

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -9,22 +9,26 @@ public class Test {
 		builder.append("},");
 		builder.append("\"zones\":[");
 		builder.append("{");
-		builder.append("\"arrival\":\"2015-04-21T11:42:11.000+02:00\",");
-		builder.append("\"departure\":\"2015-04-21T11:58:32.000+02:00\",");
 		builder.append("\"latitude\":55.61592,");
-		builder.append("\"longitude\":12.987113");
+		builder.append("\"longitude\":12.987113,");
+		builder.append("\"radius\":30,");
+		builder.append("\"material\":\"Dirt\",");
+		builder.append("\"arrival\":\"2015-04-21T11:42:11.000+02:00\",");
+		builder.append("\"departure\":\"2015-04-21T11:58:32.000+02:00\"");
 		builder.append("}");
 		builder.append("],");
 		builder.append("\"materials\":[");
 		builder.append("\"Dirt\",");
 		builder.append("\"Dirt\",");
-		builder.append("\"DiamondPickaxe\"");
+		builder.append("\"Stone\"");
 		builder.append("],");
 		builder.append("\"unload\":\"2015-04-21T13:04:54.000+02:00\"");
 		// builder.append("}");
 		builder.append("}");
 
-		RequestPoster.doPost("http://192.168.10.186:7070/unload", builder.toString());
+		System.out.println(builder.toString());
+
+		RequestPoster.doPost("http://localhost:7070/unload", builder.toString());
 	}
 
 }
